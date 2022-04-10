5df7159067f68527b86a03a9d13f1cb0cbc0022f
Alexander Zashev <azashev93@gmail.com>
2022-04-10 13:28:00 +0300

    ~

---
+++ chars_to_string.py
@@ -0,0 +1,7 @@
+ch1 = input()
+ch2 = input()
+ch3 = input()
+
+cctn = ch1 + ch2 + ch3
+
+print(cctn)
---
+++ sum_of_chars.py
@@ -0,0 +1,9 @@
+lines = int(input())
+
+sum = 0
+
+for i in range(lines):
+    char = input()
+    sum += ord(char)
+
+print(f"The sum equals: {sum}")

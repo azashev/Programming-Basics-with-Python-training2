30bfc6d04e7f37f30d4a1c17e09e7ad9b0db97ed
Alexander Zashev <azashev93@gmail.com>
2022-04-22 20:34:49 +0300

    ~

---
+++ min_max_and_sum.py
@@ -0,0 +1,13 @@
+def min_number(n):
+    print(f"The minimum number is {min(n)}")
+    print(f"The maximum number is {max(n)}")
+    print(f"The sum numbers is: {sum(n)}")
+
+
+given_numbers_list = input().split()
+given_numbers_int = []
+
+for i in given_numbers_list:
+    given_numbers_int.append(int(i))
+
+min_number(given_numbers_int)

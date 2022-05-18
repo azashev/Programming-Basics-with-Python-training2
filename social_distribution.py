fe01853e121e73243394174930ab90b69cd7fa05
Alexander Zashev <azashev93@gmail.com>
2022-05-18 04:18:54 +0300

    ~

---
+++ social_distribution.py
@@ -0,0 +1,32 @@
+population = [int(x) for x in input().split(', ')]
+min_wealth = int(input())
+
+wealthiest_part = 0
+
+for i in population:
+    if i > wealthiest_part:
+        wealthiest_part = i
+
+for x in range(len(population) - 1):
+    reduced_num = wealthiest_part - (min_wealth - population[x])
+    if population[x] < min_wealth:
+        if reduced_num >= min_wealth:
+            for num in range(len(population)):
+                if population[num] == wealthiest_part:
+                    population[num] -= min_wealth - population[x]
+                    wealthiest_part -= min_wealth - population[x]
+            population[x] += min_wealth - population[x]
+            for i in population:
+                if i > wealthiest_part:
+                    wealthiest_part = i
+
+is_valid = True
+
+for element in population:
+    if element < min_wealth:
+        is_valid = False
+
+if is_valid:
+    print(population)
+else:
+    print("No equal distribution possible")
